e49ccaa9e86fabf5b835a54bd3bddb8faf18ac1a
Eze234 <755ezgamil@gmail.com>
2025-02-01 18:43:24 -0300

    🚬 fix: handle errors in lanyard api request

--- src/utils/lanyard.ts
+++ src/utils/lanyard.ts
@@ -3,9 +3,9 @@ import axios from "axios";
 export async function discordData(id: String) {
     if (!id) throw new Error("Error, pls give a Discord ID");
 
-    const request = await axios.get(`https://api.lanyard.rest/v1/users/${id}`)
+    const request = await axios.get(`https://api.lanyard.rest/v1/users/${id}`).catch(() => null);
     
-    if (!request.data.success) throw new Error("Error in lanyard api");
+    if (!request || !request.data.success) throw new Error("Error in lanyard api");
 
     return request.data.data;
 };
