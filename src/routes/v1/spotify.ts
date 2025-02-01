9307e610227e44fd93dfb38b47b0e696925beea5
Eze234 <755ezgamil@gmail.com>
2025-02-01 18:38:14 -0300

    feat: add image route to generate Spotify album art with canvas

--- src/routes/v1/spotify.ts
+++ src/routes/v1/spotify.ts
@@ -4,6 +4,10 @@ import {
 import Package from "../../../package.json";
 import { spotify } from "../../utils/spotify";
 import { config } from "dotenv";
+import {
+    createCanvas,
+    loadImage
+} from "canvas";
 config();
 const router = Router();
 
@@ -31,6 +35,60 @@ router.route("/text/:id").get(async function(req, res) {
     } else {
         res.send(`[Nothing](${process.env.spotify || "https://spotify.com"})`)
     }
+})
+
+router.route("/image/:id").get(async function(req, res) {
+    if (!req.params.id) {
+        res.status(400).json({
+            ok: false,
+            message: "Por favor agregue una ID de discord y asegurate de estar en el discord de lanyard.\nhttps://discord.gg/9d4nuRD74x"
+        });
+        return;
+    };
+
+    const data: false | { song: string, author: string, url: string, album_art_url: string } = await spotify(req.params.id) || false;
+
+    if (data && data.author) {
+        const background = data.album_art_url;
+        const width = 800;
+        const height = 400;
+        const canvas = createCanvas(width, height);
+        const ctx = canvas.getContext("2d");
+        const image = await loadImage(background);
+        
+        ctx.drawImage(image, 0, 0, width, height);
+        ctx.fillStyle = "rgba(0, 0, 0, 0.7)";
+        ctx.fillRect(0, height - 100, width, 100);
+
+        ctx.font = "40px Arial";
+        ctx.fillStyle = "white";
+        ctx.fillText("Spotify", 630, height - 20);
+        ctx.font = "bold 40px Arial";
+        ctx.fillText(`${data?.song || "Loca"}`, 20, height - 20);
+        ctx.font = "bold 40px Arial";
+        ctx.fillText(`${data?.author || "C.R.O"}`, 20, height - 60);
+        const buffer = canvas.toBuffer();
+        res.set("Content-Type", "image/png");
+        res.send(buffer);
+    } else {
+        const width = 800;
+        const height = 400;
+        const canvas = createCanvas(width, height);
+        const ctx = canvas.getContext("2d");
+        
+        ctx.fillStyle = "rgba(0, 0, 0, 0.7)";
+        ctx.fillRect(0, height - 100, width, 100);
+
+        ctx.font = "40px Arial";
+        ctx.fillStyle = "white";
+        ctx.fillText("Spotify", 630, height - 20);
+        ctx.font = "bold 60px Arial";
+        ctx.fillText("Nothing", 20, height - 20);
+        const buffer = canvas.toBuffer();
+        res.set("Content-Type", "image/png");
+        res.send(buffer);
+    }
+
 })
 
 export default router
--- src/utils/spotify.ts
+++ src/utils/spotify.ts
@@ -8,7 +8,8 @@ export async function spotify(id: String) {
         return {
             song: spotiData?.song || "Spotify Artist",
             author: spotiData?.artist,
-            url: `https://open.spotify.com/track/${spotiData.track_id}`
+            url: `https://open.spotify.com/track/${spotiData.track_id}`,
+            album_art_url: spotiData?.album_art_url || "https://cdn.discordapp.com/attachments/848181072835772466/1335348234813702154/image.png?ex=679fd774&is=679e85f4&hm=be3f8f7939ad49df675fee5de20746cbff11361f03a2247329bff11b11f6cfe9&"
         };
     } else {
         return false;
